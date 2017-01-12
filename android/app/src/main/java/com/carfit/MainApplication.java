b63b1c84deb0e9f52deb7f2cb9749f3043deca5e
Brian McBride <brian.mcbride@zeppidy.com>
2017-01-12 02:32:11 -0800

    Scaffolding - iOS and Android Build

--- android/app/src/main/java/com/carfit/MainApplication.java
+++ android/app/src/main/java/com/carfit/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
+import com.amazonaws.reactnative.lambda.AWSRNLambdaPackage;
+import com.amazonaws.reactnative.core.AWSRNCorePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+            new MainReactPackage(),
+            new RealmReactPackage(),
+            new AWSRNLambdaPackage(),
+            new AWSRNCorePackage()
       );
     }
   };
