163791923e92ba42306428c7c9bd24ec083ef6c3
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-03-19 13:00:03 -0700

    Change payload

--- src/modules/carInstalltion/CarInstallationState.js
+++ src/modules/carInstalltion/CarInstallationState.js
@@ -34,6 +34,11 @@ export default function CarInstallationStateReducer(state = initialState, action
     case LOADING:
       return state.set('loading', action.payload);
 
+    case ADD_VEHICLE:
+      return Object.assign({}, state, {
+        vin: action.payload
+      });
+
     default:
       return state;
   }
