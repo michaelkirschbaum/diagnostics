68a0a63e0b6af17cc5d2f1ecef37aa834a19b660
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-03-23 18:12:51 -0700

    Revert "Downgrade sdk"
    This reverts commit 77a796bf8321cd723b221133e8c3da675b61aa1e.

--- src/modules/home/HomeView.js
+++ src/modules/home/HomeView.js
@@ -88,9 +88,9 @@ const HomeView = React.createClass({
 
     const vin = store.getState().get("carInstallation").get("vin");
     // backlog types: recall, maintenance, alert, bulletin
-    // var alerts = await vehicle.getAlerts('alert', vin);
+    var alerts = await vehicle.getAlerts('alert', vin);
 
-    if (false) {
+    if (alerts) {
       // set alerts
       this.setState({alerts});
     } else {
@@ -102,11 +102,11 @@ const HomeView = React.createClass({
     var vehicle = new Vehicle();
 
     const vin = store.getState().get("carInstallation").get("vin");
-    // var mileage = await vehicle.getMileage(vin);
+    var mileage = await vehicle.getMileage(vin);
 
-    if (false) {
+    if (mileage) {
       var units = 'm';
-      // mileage += units;
+      mileage += units;
 
       this.setState({mileage});
     } else {
