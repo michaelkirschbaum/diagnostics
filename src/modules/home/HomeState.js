24f1d488acbcc23ac254ff0bf8062b682725fa01
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-03-26 13:58:27 -0700

    add HomeState.js

---
+++ src/modules/home/HomeState.js
@@ -0,0 +1,74 @@
+import {Map} from 'immutable';
+import fetch from 'isomorphic-fetch';
+
+const initialState = Map({
+  odometer: {
+    isFetching: false,
+    didInvalidate: false,
+    meters: 0
+  }
+});
+
+// actions
+const REQUEST_ODOMETER = 'GET_ODOMETER';
+const RECEIVE_ODOMETER = 'RECEIVE_ODOMETER';
+const REJECT_ODOMETER = 'REJECT_ODOMETER';
+const INVALIDATE_ODOMETER= 'INVALIDATE_ODOMETER';
+
+// action creators
+export function requestOdometer() {
+  return {
+    type: REQUEST_ODOMETER
+  }
+
+export function receiveOdometer(meters) {
+  return {
+    type: RECEIVE_ODOMETER,
+    meters
+  }
+}
+
+export function rejectOdometer() {
+  return {
+    type: REJECT_ODOMETER
+  }
+}
+
+export function invalidateOdometer() {
+  return {
+    type: INVALIDATE_ODOMETER
+  }
+}
+
+export function fetchOdometer() {
+  return function(dispatch) {};
+}
+
+// reducer
+export default function odometer(state = initialState, action) {
+  switch(action.type) {
+    case REQUEST_ODOMETER:
+      return Object.assign({}, state, {
+        isFetching: true,
+        didInvalidate: false
+      });
+
+    case RECEIVE_ODOMETER:
+      return Object.assign({}, state, {
+        isFetching: false,
+        didInvalidate: false,
+        meters: action.meters
+      });
+
+    case REJECT_ODOMETER:
+      return Object.assign({}, state, {});
+
+    case INVALIDATE_ODOMETER:
+      return Object.assign({}, state, {
+        didInvalidate: true
+      });
+
+    default:
+      return state;
+  }
+}
