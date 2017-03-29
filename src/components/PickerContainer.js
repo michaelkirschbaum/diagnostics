df3425077e112f60969dce8dc71e753c38fd37ca
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-03-29 16:00:55 -0700

    add PickerContainer.js

---
+++ src/components/PickerContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Content, Picker } from 'native-base';
+const Item = Picker.Item;
+
+const PickerContainer = React.createClass({
+  render() {
+    
+  }
+});
+
+export default PickerContainer;
