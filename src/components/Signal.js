3a61d8b8a3900fb12b853762168ea231126e5c9d
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-03-29 10:23:37 -0700

    route phone button

--- src/components/Signal.js
+++ src/components/Signal.js
@@ -6,21 +6,21 @@ class Signal extends Component {
     let strength = this.props.strength;
 
     // signal increases as strength approaches 0
-    if (strength >= -25)
+    if (strength <= .25)
       return(
-        <Image source={require('../../images/icons/wifi3.png')} resizeMode='center'/>
+        <Image source={require('../../images/icons/wifi0.png')} resizeMode='center'/>
       );
-    else if (strength >= -50)
-      return(
-        <Image source={require('../../images/icons/wifi2.png')} resizeMode='center'/>
-      );
-    else if (strength >= -75)
+    else if (strength <= .50)
       return(
         <Image source={require('../../images/icons/wifi1.png')} resizeMode='center'/>
       );
+    else if (strength <= .75)
+      return(
+        <Image source={require('../../images/icons/wifi2.png')} resizeMode='center'/>
+      );
     else
       return(
-        <Image source={require('../../images/icons/wifi0.png')} resizeMode='center'/>
+        <Image source={require('../../images/icons/wifi3.png')} resizeMode='center'/>
       );
   };
 }
