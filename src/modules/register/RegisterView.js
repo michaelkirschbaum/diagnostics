f825f97336886a951dd8e36178934cbd76a80c66
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-04-06 15:17:42 -0700

    Register view style

--- src/modules/register/RegisterView.js
+++ src/modules/register/RegisterView.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {
+  Container,
+  Header,
+  Button,
+  Icon,
+  Title,
+  Content,
+  View
+} from 'native-base';
+import carfitTheme from '../../config/carfit-theme';
+import loc from '../../config/localization';
+import colors from '../../config/colors';
 
 const RegisterView = React.createClass({
-  render() {}
+  render() {
+    return (
+      <Container theme={carfitTheme}>
+        <Header>
+          <Button transparent onPress={() => this.props.onNavigateBack()}>
+            <Icon name="ios-arrow-back"/>
+          </Button>
+          <Title>{headerTitle}</Title>
+        </Header>
+        <Content style={{backgroundColor: colors.backgroundPrimary}}>
+        </Content>
+      </Container>
+    );
+  }
 });
 
 export default RegisterView;
