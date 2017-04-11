22ff20516a4fa1718c33444556c4705866f67a91
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-04-11 09:16:52 -0700

    Add style to czonnectionspinner

--- src/components/ConnectionSpinner.js
+++ src/components/ConnectionSpinner.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import { Image, View, StyleSheet } from 'react-native';
 
 const ConnectionSpinner = React.createClass({
   render() {
+    let images = null;
+
     return (
       <View>
-        <Image source={require('../../images/Connected.png')}/>
+        <Image source={require('../../images/Connecting1.png')}/>
       </View>
     );
-  }
+  },
+
+  componentDidMount() {}
+});
+
+const styles = StyleSheet.create({
+  spinner: {}
 });
 
 export default ConnectionSpinner;
