1820b80ccb0242ed3b80087e78800476f861f376
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-04-17 19:21:16 -0700

    fix bug

--- src/modules/details/DetailsView.js
+++ src/modules/details/DetailsView.js
@@ -62,10 +62,10 @@ const DetailsView = React.createClass({
 
     let headerTitle = loc.myCars.myCars;
 
-    let vehicle = store.getState().get("carInstallation").get("vehicles").last();
+    let vehicle = store.getState().get("carInstallation").get("vehicles").slice(-1)[0];
 
-    let name = vehicle.get("name");
-    let mileage = this.convertMeters(vehicle.get("current_meters"));
+    let name = vehicle.name;
+    let mileage = this.convertMeters(vehicle.current_meters);
     let image = '';
 
     let connected = this.props.connected ? "Connected" : "Disconnected";
@@ -73,16 +73,16 @@ const DetailsView = React.createClass({
     let phone = '';
 
     let infoDetailsData = {
-      year: vehicle.get("year"),
-      make: vehicle.get("make"),
-      model: vehicle.get("model"),
-      mpgCity: this.convertMeters(vehicle.get("meters_per_liter_city")),
-      mpgHighway: this.convertMeters(vehicle.get("meters_per_liter_highway")),
+      year: vehicle.year,
+      make: vehicle.make,
+      model: vehicle.model,
+      mpgCity: this.convertMeters(vehicle.meters_per_liter_city),
+      mpgHighway: this.convertMeters(vehicle.meters_per_liter_highway),
       license: '',
-      vin: vehicle.get("vin"),
-      drivenWheels: vehicle.get("driven_wheels"),
-      trimLevel: vehicle.get("trim_level"),
-      doors: vehicle.get("num_doors"),
+      vin: vehicle.vin,
+      drivenWheels: vehicle.driven_wheels,
+      trimLevel: vehicle.trim_level,
+      doors: vehicle.num_doors
     };
 
     let infoDetails = _.map(_.toPairs(infoDetailsData), infoPairs => {
