da8244db0d500aa6b64a0e36e183116425710758
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-05-05 16:49:10 -0700

    Fix bug

--- src/modules/carInstalltion/CarInstallationView.js
+++ src/modules/carInstalltion/CarInstallationView.js
@@ -403,7 +403,7 @@ const CarInstallationStateView = React.createClass({
     this.props.setOdometerModal(true);
 
     if (this.locationFrance())
-      this.props.pushRoute({key: 'Home', title: loc.settings.settings});
+      this.props.switchRoute('MainRoute');
     else
       this.props.switchRoute(2);
   },
