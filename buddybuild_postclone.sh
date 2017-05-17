f5e6865a7da793c79b1afcf64542773ac925cb0f
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-05-17 12:25:50 -0700

    fix bug

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,6 +3,3 @@
 pod repo add Pods https://bitbucket.org/carfit_platform/pods.car.fit.git
 
 sudo gem install xcodeproj
-watchman-watch-del-all
-rm -rf node_modules && npm install
-npm start -- --reset-cache
