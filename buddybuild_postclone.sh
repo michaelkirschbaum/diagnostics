af74e1170772b31fae0155ebcded2dc50fa278e4
Nabyl Bennouri <nbennouri@car.fit>
2017-05-18 15:23:12 -0700

    TestFlight settings

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,8 +3,6 @@
 pod repo add Pods https://bitbucket.org/carfit_platform/pods.car.fit.git
 
 sudo gem install xcodeproj
-brew update
-brew install watchman
 watchman watch-del-all
 rm -rf node_modules && npm install
 rm -fr $TMPDIR/react-*
