572be139959ffd7c25ad4c08e5f7cba00bae2ec8
Nabyl Bennouri <nbennouri@car.fit>
2017-05-18 11:25:56 -0700

    Watchman fix

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -5,7 +5,7 @@ pod repo add Pods https://bitbucket.org/carfit_platform/pods.car.fit.git
 sudo gem install xcodeproj
 brew update
 brew install watchman
-watchman-watch-del-all
+watchman watch-del-all
 rm -rf node_modules && npm install
 npm start -- --reset-cache
 
