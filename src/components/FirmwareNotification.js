26b078c7b1ac93f7283578b9b304cac5797f6e5a
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-05-19 17:11:27 -0700

    firmwarenotification style

--- src/components/FirmwareNotification.js
+++ src/components/FirmwareNotification.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import {Text} from 'native-base';
+import {Text, Content} from 'native-base';
+import colors from '../config/colors';
 
 export default class FirmwareNotification extends Component {
   render() {
     return (
-      <Text style={styles.text}>Device update: {this.props.percent}</Text>
+      <Content style={styles.container}>
+        <Text style={styles.text}>Device update: {this.props.percent}</Text>
+      </Content>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primary
+  },
   text: {
     textAlign: 'center'
   }
