eb4670add13936e07d964b88b98db6bdfe174665
Michael Kirschbaum <hi@michaelkirschbaum.com>
2017-05-31 14:27:27 -0700

    remove db name

--- src/modules/login/LoginView.js
+++ src/modules/login/LoginView.js
@@ -55,7 +55,7 @@ const LoginView = React.createClass({
 
       auth0
         .authentication("t2mDZ2JX86H2iKiM9QhAutQkgHo0x42M")
-        .login(this.state.email, this.state.password, "myconnection")
+        .login(this.state.email, this.state.password)
         .then(credentials => login.auth0("carfit.auth0.com", credentials))
         .catch(error => console.log(error));
 
